944df56f11934178a4b622f26bed75f8eadf8a81
cordesign <vasco.feeney@icloud.com>
2016-03-26 14:52:44 +0000

    update
    Added main planet with radiuos being followers and color being profile
    color
    neighbour planets, amount is number of mentions and their size and color
    same principle as main planet
    satelties, amount is the number of tweets

--- js/planets.js
+++ js/planets.js
@@ -4,18 +4,7 @@ THREEx.Planets	= {}
 
 THREEx.Planets.baseURL	= '../'
 
-THREEx.Planets.createStarfield	= function(){
-	var texture	= THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/galaxy_starfield.png')
-	var material	= new THREE.MeshBasicMaterial({
-		map	: texture,
-		side	: THREE.BackSide
-	})
-	var geometry	= new THREE.SphereGeometry(10, 32, 32)
-	var mesh	= new THREE.Mesh(geometry, material)
-	return mesh	
-}
-
-function planet() {
+function planet(r, c, tCount, mCount) {
    var renderer	= new THREE.WebGLRenderer({
 		antialias	: true
 	});
@@ -27,9 +16,52 @@ function planet() {
 	var scene	= new THREE.Scene();
 	var camera	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100 );
 	camera.position.z = 1;
-	
-	var starSphere	= THREEx.Planets.createStarfield()
+
+    var light	= new THREE.DirectionalLight( 0xffffff , 1 )
+	light.position.set(5,5,5)
+	scene.add( light );
+    	
+    var starSphere	= THREEx.Planets.createStarfield()
 	scene.add(starSphere)
+    
+    for(var i = 0; i < mCount; i++){
+        
+        min = -3;
+        max = 3;
+
+        
+        X = Math.random() * (max - min) + min
+        Y = Math.random() * (max - min) + min
+        Z = Math.random() * (max - min) + min
+            
+        mentions = JSON.parse(localStorage.getItem(i))
+        r2 = mentions[0]
+        c2 = mentions[1]
+        var moon = THREEx.Planets.createMoon(r2, c2);
+        moon.position.set(X,Y,Z)
+        scene.add(moon);
+    }
+    
+    for(var i = 0; i < tCount; i++){
+        
+        min = -1;
+        max = 1;
+
+        
+        X = Math.random() * (max - min) + min
+        Y = Math.random() * (max - min) + min
+        Z = Math.random() * (max - min) + min
+            
+        var moon = THREEx.Planets.createTweet();
+        moon.position.set(X,Y,Z)
+        moon.scale.multiplyScalar(1/10)
+        scene.add(moon);
+    }
+    
+    
+    
+    var earthMesh = THREEx.Planets.createEarth(r, c);
+    scene.add(earthMesh)
     
 	var mouse	= {x : 0, y : 0}
 	document.addEventListener('mousemove', function(event){
@@ -61,4 +93,66 @@ function planet() {
 	})
 }
 
-planet();
+THREEx.Planets.createStarfield	= function(){
+	var texture	= THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/galaxy_starfield.png')
+	var material	= new THREE.MeshBasicMaterial({
+		map	: texture,
+		side	: THREE.BackSide
+	})
+	var geometry	= new THREE.SphereGeometry(70, 32, 32)
+	var mesh	= new THREE.Mesh(geometry, material)
+	return mesh	
+}
+
+THREEx.Planets.createEarth = function(r, profile_color) {
+    
+    var geometry = new THREE.SphereGeometry(r,32,32)
+    var material = new THREE.MeshPhongMaterial({
+        color: "#" + profile_color
+    })
+    var mesh = new THREE.Mesh(geometry, material)
+    
+    return mesh
+    
+}
+THREEx.Planets.createTweet = function(profile_color) {
+    var geometry = new THREE.SphereGeometry(0.1, 32 , 32)
+    var material = new THREE.MeshPhongMaterial({
+        color: "#" + profile_color
+    })
+    var mesh = new THREE.Mesh(geometry, material)
+    
+    return mesh
+}
+
+THREEx.Planets.createMoon = function(r, c) {
+    var geometry = new THREE.SphereGeometry(r, 32 , 32)
+    var material = new THREE.MeshPhongMaterial({
+        color: "#" + c
+    })
+    
+    var mesh = new THREE.Mesh(geometry, material)
+    
+    return mesh
+}
+
+
+
+function lights(light) { 
+	light.castShadow	= true
+	light.shadowCameraNear	= 0.01
+	light.shadowCameraFar	= 15
+	light.shadowCameraFov	= 45
+	light.shadowCameraLeft	= -1
+	light.shadowCameraRight	=  1
+	light.shadowCameraTop	=  1
+	light.shadowCameraBottom= -1
+	// light.shadowCameraVisible	= true
+	light.shadowBias	= 0.001
+	light.shadowDarkness	= 0.2
+	light.shadowMapWidth	= 1024
+	light.shadowMapHeight	= 1024
+
+}
+
+//planet(0.5, 0x0084b4, 10)
--- js/twitter.js
+++ js/twitter.js
@@ -1,28 +1,56 @@
-
+if (typeof(Storage) !== "undefined") {
 //create an account in oauth.io
 OAuth.initialize('hQkWf-Woke6ArXBFHkxpDrJo2-Q')
 //this makes a window pop up to authorize our app
 OAuth.popup('twitter', {cache: true}).done(function(twitter) {
 
+    var q = "";
     twitter.get('https://api.twitter.com/1.1/account/verify_credentials.json').done(function(data) {   
-        console.log(data)
-        
-   })
-    
+        r = 0.1 * Math.log(data.followers_count);
+        color = data.profile_link_color; 
+        mentionsCount = data.status.entities.user_mentions.length
+        localStorage.setItem("count", mentionsCount)
+         localStorage.setItem("user", JSON.stringify([color, r,mentionsCount]));
+
+            for(i = 0; i < mentionsCount; i++){ 
+                if(i+1 < mentionsCount) {
+                    q += data.status.entities.user_mentions[i].screen_name +","
+                } else {
+                    q += data.status.entities.user_mentions[i].screen_name 
+                }
                 
-twitter.get('https://api.twitter.com/1.1/followers/list.json').done(function(data) {
+                localStorage.setItem("q", JSON.stringify([q]));
+            }
         
-        console.log(data)    
-    
-    })
-
-    twitter.get('http://api.wunderground.com/api/778255f9460ead52/geolookup/q/autoip.json').done(function (data) {
+            twitter.get("https://api.twitter.com/1.1/users/lookup.json?screen_name=" + q).done(function(data) { 
+                for(i = 0; i < mentionsCount; i++) {
+                    
+                    r2 = 0.1 * Math.log(data[i].followers_count);
+                    color2 = data[i].profile_link_color;
+                    
+                    localStorage.setItem(i, JSON.stringify([r2, color2]));
+                }
+               
+                
         console.log(data)
-        twitter.get('http://api.wunderground.com/api/778255f9460ead52/forecast/q/'+ data.location.country_name + '/'+ data.location.city + '.json').done(function (data) {
-                console.log(data);
-            })
-
-    })
+                 })
+          })
+      
+   twitter.get("https://api.twitter.com/1.1/statuses/user_timeline.json").done(function(data){
+       
+       localStorage.setItem("tCount", data.length)
+       
+   })
 });
-
+            user = JSON.parse(localStorage.getItem("user"))
+            color = user[0]
+            mCount = user[2]
+            r = user[1]
+            tCount = localStorage.getItem("tCount")
+            
+            planet(r, color, tCount, mCount)
+            // Retrieve
+            } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+        } 
 
